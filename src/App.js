c8db6fdf623b4edb9053115f57a268abe46ff6f0
minju <amilk5u@naver.com>
2024-07-14 17:24:20 +0900

    docs : 타이틀 'Moive App' 변경

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
+import styled from "styled-components";
 
 function App() {
   return (
-    <Router>
-      {/* Switch는 if 같은 조건문 인듯 */}
-      <Switch> 
-        <Route path="/abot-us">
-          <h1>Hello</h1>
-        </Route>
-        <Route path="/movie/:id">
-          <Detail />
-        </Route>
-        <Route path="/">
-          <Home /> 
-        </Route>
-      </Switch>
-    </Router>
+    <>
+      <Title>Movie App</Title>
+      <Router>
+        {/* Switch는 if 같은 조건문 인듯 */}
+        <Switch>
+          <Route path="/abot-us">
+            <h1>Hello</h1>
+          </Route>
+          <Route path="/movie/:id">
+            <Detail />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
-
 export default App;
+
+const Title = styled.h1`
+  width: fit-content;
+  margin: 50px auto 10px;
+  font-size: 40px;
+  color: #222;
+`
